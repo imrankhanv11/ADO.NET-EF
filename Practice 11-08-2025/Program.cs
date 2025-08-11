5feccbdc847ce36784579baaf26d10ccf0b35574
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-11 18:12:28 +0530

    Add files via upload

--- Practice 11-08-2025/Program.cs
+++ Practice 11-08-2025/Program.cs
@@ -38,21 +38,52 @@ class Practice
                 object totalage = cmd.ExecuteScalar();
                 Console.WriteLine("Using ExcuteScalar");
                 Console.WriteLine("The Total Age :" + totalage);
+
                 // Excutenonscalar
                 //------------ INSERT --------------------
                 string queryinsert = "INSERT INTO Students (Name, Age, Course) VALUES ('Imran Khan', 22, 'B.Tech AI & DS'), ('John Smith', 23, 'BCA')";
                 SqlCommand cmd2 = new SqlCommand(queryinsert, conn2);
                 int affectedrow = cmd2.ExecuteNonQuery();
                 Console.WriteLine(affectedrow);
+
                 // ------------ DELETE --------------------
                 string deletequery = "Delete Students where id=5";
                 SqlCommand cmd3 = new SqlCommand(deletequery, conn2);
                 int deletedRows = cmd3.ExecuteNonQuery();
                 Console.WriteLine(deletedRows);
+
+                //--------- UPDATE ------------------------
+                string update = "UPDATE Students SET Name = 'Update' WHERE Id = 2";
+                SqlCommand cmd4 = new SqlCommand(update, conn2);
+                int updaterows = cmd4.ExecuteNonQuery();
+                Console.WriteLine(updaterows);
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error:" + ex.Message);
+            }
+        }
+
+        using (SqlConnection newcon = new  SqlConnection(connectionString))
+        {
+            try
+            {
+                newcon.Open();
+                Console.Write("Enter the Id to Delete: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                string sqldelteid = "DELETE FROM Students WHERE Id = @Id";
+                SqlCommand cmd = new SqlCommand(sqldelteid, newcon);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                int Delterows = cmd.ExecuteNonQuery();
+                Console.WriteLine(Delterows + " row(s) deleted.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error :" + ex.Message);
             }
         }
     }
