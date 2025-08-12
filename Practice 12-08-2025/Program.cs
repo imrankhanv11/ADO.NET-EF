48d62788371dad48109ec6282b502a1b3f8d3c2c
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-12 10:50:03 +0530

    Add files via upload

---
+++ Practice 12-08-2025/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Practice12
+{
+    class mainpractice
+    {
+        public static void Main(string[] args)
+        {
+            Practice obj = new Practice();
+            CRUD crudobj = new CRUD();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("---------- MENU ---------");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1.             TABLE     ");
+            Console.WriteLine("2.             CRUD      ");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Enter the Code to run :");
+            int code = Convert.ToInt32(Console.ReadLine());
+
+            switch(code)
+            {
+                case 1:
+                    obj.Run();
+                    break;
+                case 2:
+                    crudobj.crudmethod();
+                    break;
+                default:
+                    Console.WriteLine("Try again");
+                    break;
+            }
+
+        }
+    }
+}
