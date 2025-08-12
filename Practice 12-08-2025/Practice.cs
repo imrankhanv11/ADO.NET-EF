13448037f5d07570f9da67eadd914c0ac4bdc138
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-12 10:50:24 +0530

    Add files via upload

---
+++ Practice 12-08-2025/Practice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Practice12
+{
+    internal class Practice
+    {
+        string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
+
+        [Obsolete]
+        public void Run()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Create Table
+                    //string createTableQuery = @"
+                    //CREATE TABLE Employee (
+                    //    Id INT PRIMARY KEY IDENTITY(1,1),
+                    //    Name NVARCHAR(50),
+                    //    Age INT,
+                    //    Department NVARCHAR(50)
+                    //)";
+
+                    //SqlCommand command = new SqlCommand(createTableQuery, connection);
+                    //command.ExecuteNonQuery();
+                    //Console.WriteLine("Table created successfully.");
+
+                    // Insert Data
+                    string insertData = "Insert Into Employee (Name, Age, Department) Values ('Imran', 21, 'Software'),('Khan', 22,'Finance')";
+                    SqlCommand insertCommand = new SqlCommand(insertData, connection);
+                    int insertRow = insertCommand.ExecuteNonQuery();
+                    Console.WriteLine("Inserted Rows :" + insertRow);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
