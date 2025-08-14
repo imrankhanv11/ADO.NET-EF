602a234fccda1db1df9f921a70a1af6347559a1c
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-14 16:46:03 +0530

    Add files via upload

---
+++ DBFirstApproach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirstApproach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read
+            CrudOp crud = new CrudOp();
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("-------------| Menu |------------");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("| 1.                 Main       |");
+            Console.WriteLine("| 5.                 EXIT       |");
+            Console.WriteLine("---------------------------------");
+
+            while(true)
+            {
+                Console.Write("Enter the Code to Run : ");
+                string code = Console.ReadLine();
+
+                switch (code)
+                {
+                    case "1":
+                        crud.crudmethod();
+                        break;
+                    case "2":
+                        Console.WriteLine("Thank you");
+                        return;
+                    default:
+                        Console.WriteLine("Try again");
+                        break;
+                }
+            }
+
+        }
+    }
+}
