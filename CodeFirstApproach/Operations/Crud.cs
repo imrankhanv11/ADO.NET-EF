cdcbf04b89ecb3002125a11b47db277e45b7ece2
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-19 11:46:16 +0530

    Add files via upload

---
+++ CodeFirstApproach/Operations/Crud.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproach.Operations
+{
+    internal class Crud
+    {
+        public void crudoperaion()
+        {
+            while (true)
+            {
+                Console.WriteLine("1.     Read    ");
+                Console.WriteLine("2.     Insert  ");
+                Console.WriteLine("3.     Update  ");
+                Console.WriteLine("4.     Delete  ");
+                Console.WriteLine();
+                Console.Write("enter the code : ");
+                string code = Console.ReadLine();
+
+                switch (code)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    case "5":
+                        Console.WriteLine("Thank you");
+                        return;
+                    default:
+                        Console.WriteLine("Try again");
+                        break;
+                }
+            }
+        }
+    }
+}
