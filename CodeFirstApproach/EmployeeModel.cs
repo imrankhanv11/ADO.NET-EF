9fae747b2663dea05fd88896eafd55245fc3b381
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-19 11:11:41 +0530

    Add files via upload

---
+++ CodeFirstApproach/EmployeeModel.cs
@@ -0,0 +1,23 @@
+using CodeFirstApproach.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace CodeFirstApproach
+{
+    public class EmployeeModel : DbContext
+    {
+        public EmployeeModel()
+            : base("name=Employee")
+        {
+        }
+
+        public DbSet<Employees> Employee {  get; set; }
+        public DbSet<Department> department { get; set; }
+
+        public DbSet<Projects> Projects { get; set; }
+        public DbSet<EmployeeProject> EmployeesProjects {  get; set; }
+
+    }
+
+}
