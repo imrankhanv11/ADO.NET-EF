653d0863f4fa4dfb0fc76519a7eb814661616145
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-20 17:13:30 +0530

    Add files via upload

---
+++ CodeFirst_EFCore_01/EmployeeProject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproach.Models
+{
+    public class EmployeeProject
+    {
+        public int EmpID { get; set; }
+        public int ProjectID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Roll { get; set; }
+
+        public virtual Employees Employees { get; set; }
+        public virtual Projects Projects { get; set; }
+    }
+
+}
---
+++ CodeFirst_EFCore_01/Projects.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproach.Models
+{
+    public class Projects
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public int Budget {  get; set; }
+
+        public virtual ICollection<EmployeeProject> Employees { get; set; }
+
+        public Projects()
+        {
+            Employees = new HashSet<EmployeeProject>();
+        }
+    }
+}
