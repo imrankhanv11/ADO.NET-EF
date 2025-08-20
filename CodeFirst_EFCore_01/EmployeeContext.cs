53401408d9733264a40e28a59071a0efb66097f8
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-20 17:13:05 +0530

    Add files via upload

---
+++ CodeFirst_EFCore_01/EmployeeContext.cs
@@ -0,0 +1,45 @@
+﻿using CodeFirst_EFCore_01.DTO;
+using CodeFirstApproach.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_EFCore_01
+{
+    public class EmployeeContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                var connectionString = config.GetConnectionString("MyDb");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public DbSet<Employees> Employees {  get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<EmployeeProject> EmployeesProject { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+        public DbSet<spEmployee> spEmployee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EmployeeProject>()
+                .HasKey(ep => new { ep.EmpID, ep.ProjectID });
+
+            modelBuilder.Entity<spEmployee>().HasNoKey();
+        }
+    }
+}
---
+++ CodeFirst_EFCore_01/Program.cs
@@ -0,0 +1,69 @@
+﻿using CodeFirst_EFCore_01.DTO;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CodeFirst_EFCore_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter the code : ");
+                string code = Console.ReadLine();
+
+                switch(code)
+                {
+                    case "1":
+                        sp();
+                        break;
+                    case "2":
+                        spoutput();
+                        return;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void sp()
+        {
+            using(var db = new EmployeeContext())
+            {
+                int deptId = 1;
+                var deptIdParam = new SqlParameter("@DepartmentId", deptId);
+
+                var employees = db.spEmployee
+                    .FromSqlRaw("EXEC GetEmployeesByDepartment @DepartmentId", deptIdParam)
+                    .ToList();
+
+                foreach (var emp in employees)
+                {
+                    Console.WriteLine(emp.Name);
+                }
+            }
+        }
+
+        public static void spoutput()
+        {
+            using (var db = new EmployeeContext())
+            {
+                var countParam = new SqlParameter
+                {
+                    ParameterName = "@Output",  
+                    SqlDbType = System.Data.SqlDbType.Int,
+                    Direction = System.Data.ParameterDirection.Output
+                };
+
+                db.Database.ExecuteSqlRaw("EXEC GetCountEmployee @Output OUTPUT", countParam);
+
+                int output = (int)countParam.Value;
+
+                Console.WriteLine("Total Employees: " + output);
+            }
+        }
+
+    }
+}
