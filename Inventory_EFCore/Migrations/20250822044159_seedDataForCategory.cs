c3a4b63553102409aaa97d5bed6781f4c2ccc121
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-22 17:38:29 +0530

    Add files via upload

---
+++ Inventory_EFCore/Migrations/20250822044159_seedDataForCategory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EFCore_DBFirstApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedDataForCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "category_id", "category_name" },
+                values: new object[,]
+                {
+                    { -4, "Default 04" },
+                    { -3, "Default 03" },
+                    { -2, "Default 02" },
+                    { -1, "Default 01" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
