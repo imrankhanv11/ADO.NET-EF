ac10aa53b2a9c21e0c485e291f31e81a0e978f7b
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-22 17:39:56 +0530

    Add files via upload

---
+++ Inventory_EFCore/Program.cs
@@ -0,0 +1,65 @@
+﻿using DBFirst_EFCore_01.Models;
+using DBFirst_EFCore_01.Operations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+
+namespace DBFirst_EFCore_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            sp_operations sp = new sp_operations();
+
+            insertBulk insert = new insertBulk();
+            while (true)
+            {
+                //select
+                Console.WriteLine("1.      departmentEmp");
+                //select as count
+                Console.WriteLine("2.      departmentEmpID");
+                //input
+                Console.WriteLine("3.      employeeCount");
+                //output
+                Console.WriteLine("4.      projectCount");
+                //insertdepartment
+                Console.WriteLine("5.      InsertDepartment");
+                //list of data via dataTable --> sp ( UDTT )
+                Console.WriteLine("6       inserBulkEmp");
+                //exit
+                Console.WriteLine("7.      Exit");
+
+                Console.Write("Enter the code : ");
+                string code = Console.ReadLine();
+
+                switch (code) {
+                    case "1":
+                        sp.basicselect();
+                        break;
+                    case "2":
+                        sp.basicselect2();
+                        break;
+                    case "3":
+                        sp.departmentEmpID();
+                        break;
+                    case "4":
+                        sp.projectcount();
+                        break;
+                    case "5":
+                        sp.insertDepartment();
+                        break;
+                    case "6":
+                        insert.insertemp(); 
+                        break;
+                    case "7":
+                        Console.WriteLine("Thank You");
+                        return;
+                    default:
+                        Console.WriteLine("try agian");
+                        break;
+                }
+            }
+        }
+    }
+}
