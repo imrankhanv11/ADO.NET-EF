7ffe7234e9ad2cf4d11285e5260cd2a967578ab2
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-22 17:37:56 +0530

    Add files via upload

---
+++ Inventory_EFCore/Data/CetegoriesConfigurations.cs
@@ -0,0 +1,39 @@
+﻿using EFCore_DBFirstApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_DBFirstApp.Data
+{
+    public class CetegoriesConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category
+                {
+                    CategoryId = -1, CategoryName = "Default 01"
+                },
+                new Category
+                {
+                    CategoryId = -2, CategoryName = "Default 02"
+                },
+                new Category
+                {
+                    CategoryId = -3,
+                    CategoryName = "Default 03"
+                },
+                new Category
+                {
+                    CategoryId = -4,
+                    CategoryName = "Default 04"
+                }
+
+                );
+        }
+    }
+}
