2b725279c5b5760bceec8b71bafdcde74d27a24d
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-23 19:17:08 +0530

    Add files via upload

---
+++ Inventory_EFCore/DTO/AddProductsinWareHouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_DBFirstApp.DTO
+{
+    public class AddProductsinWareHouse
+    {
+        public int ProductID { get; set; }
+        public int WareHouseID { get; set; }
+        public int Quantity { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ Inventory_EFCore/DTO/CheckProuductstock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_DBFirstApp.DTO
+{
+    public class CheckProductStock
+    {
+        public int productid { get; set; }
+        public string productname { get; set; }
+        public int totalStock { get; set; }
+    }
+}
