53b440581cc5804812fb6d18826328b6584723ce
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-25 10:06:30 +0530

    Add files via upload

---
+++ Inventory_EFCore/DTO/SupplierName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_DBFirstApp.DTO
+{
+    public class SupplierName
+    {
+        public string Name { get; set; }
+    }
+}
