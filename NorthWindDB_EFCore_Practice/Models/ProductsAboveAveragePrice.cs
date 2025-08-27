dcb3b53d88a051286ca0f8c86b6d4540a9a4bf70
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-27 14:04:21 +0530

    Add files via upload

---
+++ NorthWindDB_EFCore_Practice/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excersice2_EFCoreNorthWind.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
