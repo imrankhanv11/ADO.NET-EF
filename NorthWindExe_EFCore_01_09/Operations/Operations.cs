79a4c77ee49b19f4a5f35d256f86ebfa57af043e
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-01 13:49:37 +0530

    Add files via upload

---
+++ NorthWindExe_EFCore_01_09/Operations/Operations.cs
@@ -0,0 +1,146 @@
+﻿using EXC_NorthWind_01_09_2025.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXC_NorthWind_01_09_2025
+{
+    public class Operations
+    {
+        public async Task EmpTerteroy()
+        {
+            try
+            {
+                using (var dbcontext = new NorthWindContext())
+                {
+                    var list = await dbcontext.Employees.Select(s => new
+                    {
+                        ID = s.EmployeeId,
+                        Name = s.FirstName + " " + s.LastName,
+                        Territory = s.Territories.Select(t => t.TerritoryDescription)
+                    }).ToListAsync();
+
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item.ID + " - " + item.Name);
+                        foreach (var item2 in item.Territory)
+                        {
+                            Console.WriteLine(item2);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task OrderWithoutOrderDetail()
+        {
+            try
+            {
+                using (var dbcontext = new NorthWindContext())
+                {
+                    var list = await dbcontext.Orders
+                        .Where(o => !o.OrderDetails.Any())
+                        .Select(o => new
+                        {
+                            o.OrderId,
+                            o.CustomerId,
+                            o.OrderDate
+                        })
+                        .ToListAsync();
+
+                    foreach (var order in list)
+                    {
+                        Console.WriteLine($"{order.OrderId} | {order.CustomerId} | {order.OrderDate}");
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public void CatWithHightProductQuntity()
+        {
+            try
+            {
+
+                using (var dbcontext = new NorthWindContext())
+                {
+                    var result = dbcontext.Categories
+                        .Select(c => new
+                        {
+                            CategoryName = c.CategoryName,
+                            TotalQuantityOrdered = c.Products
+                                .SelectMany(p => p.OrderDetails)
+                                .Sum(od => (int?)od.Quantity) ?? 0
+                        })
+                        .OrderByDescending(x => x.TotalQuantityOrdered)
+                        .FirstOrDefault();
+
+                    Console.WriteLine($"{result.CategoryName} | {result.TotalQuantityOrdered}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public async Task CusAndTheirAvgOrders()
+        {
+            using(var dbcontext = new NorthWindContext())
+            {
+                //var list = dbcontext.Customers.Select(s=> new
+                //{
+                //    ID = s.CustomerId,
+                //    Name = s.CompanyName,
+                //    Avg = s.Orders.
+                //})
+            }
+        }
+
+        public async Task ShippersWithTotalOrders()
+        {
+            try
+            {
+                using(var dbcontext = new NorthWindContext())
+                {
+                    var result = await dbcontext.Shippers
+                            .Select(s => new
+                            {
+                                ShipperName = s.CompanyName,
+                                TotalRevenueHandled = s.Orders.Sum(o =>
+                                    o.Freight +
+                                    o.OrderDetails.Sum(od => od.UnitPrice * od.Quantity * (decimal)(1 - od.Discount))
+                                )
+                            })
+                            .ToListAsync();
+
+                    foreach (var item in result)
+                    {
+                        Console.WriteLine(item.ShipperName + " --> " + item.TotalRevenueHandled);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ NorthWindExe_EFCore_01_09/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace EXC_NorthWind_01_09_2025
+{
+    public class Program
+    {
+        public static async Task Main(string [] args)
+        {
+            // obj
+            Operations one = new Operations();
+            while (true)
+            {
+                Console.WriteLine("*--------------------------------------------------------------------------------------*");
+                Console.WriteLine("|                                         #MENU                                        |");
+                Console.WriteLine("*--------------------------------------------------------------------------------------*");
+                Console.WriteLine("|  1.    |     employee’s full name along with all the territories                     |");
+                Console.WriteLine("|  2.    |      orders in the system that do not have any related OrderDetails.        |");
+                Console.WriteLine("|  3.    |     product category the highest total quantity ordered across all orders.  |");
+                Console.WriteLine("|  4.    |     customer, calculate the average number of days between their orders.    |");
+                Console.WriteLine("|  5.    |     all shippers with the total order value they handled                    |");
+                Console.WriteLine("|  6.    |     all products that were never included in any order.                     |");
+                Console.WriteLine("|  7.    |     Rank employees by their total sales amount in 1998.                     |");
+                Console.WriteLine("|  8.    |     Generate a report of total sales per month for the year 1997 by mon     |");
+                Console.WriteLine("|  9.    |     customer who had the longest gap between two consecutive orders.        |");
+                Console.WriteLine("| 10.    |     prds where the crnt stk units on order is less than the reorder level.  |");
+                Console.WriteLine("| 11.    |     list the top 3 other products most frequently ordered together with it. |");
+                Console.WriteLine("| 12.    |     10% discount to all orders placed by customers from Germany in 1997     |");
+                Console.WriteLine("| 13.    |     Delete all orders that were never shipped (ShippedDate IS NULL          |");
+                Console.WriteLine("| 14.    |     Insert a new order for an existing customer with at least 2 ord detisle |");
+                Console.WriteLine("|        |     making sure EF Core correctly saves both the order and its details.     |");
+                Console.WriteLine("|  0.    |     EXIT                                                                    |");
+                Console.WriteLine("*--------------------------------------------------------------------------------------*");
+                Console.WriteLine();
+                Console.Write("Enter the Code : ");
+                string Code = Console.ReadLine();
+
+                switch (Code)
+                {
+                    case "1":
+                        await one.EmpTerteroy();
+                        break;
+                    case "2":
+                        await one.OrderWithoutOrderDetail();
+                        break;
+                    case "3":
+                        one.CatWithHightProductQuntity();
+                        break;
+                    case "4":
+                        await one.CusAndTheirAvgOrders();
+                        break;
+                    case "5":
+                        await one.ShippersWithTotalOrders();
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+                        break;
+                    case "9":
+                        break;
+                    case "10":
+                        break;
+                    case "11":
+                        break;
+                    case "12":
+                        break;
+                    case "13":
+                        break;
+                    case "14":
+                        break;
+                        break;
+                    case "0":
+                        Console.WriteLine("Thank You");
+                        return;
+                    default:
+                        Console.WriteLine("Try again Code not found");
+                        break;
+                }
+            }
+        }
+    }
+}
